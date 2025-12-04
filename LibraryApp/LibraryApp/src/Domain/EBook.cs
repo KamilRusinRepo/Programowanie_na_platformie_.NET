afad815036407a0e2e1864071aaf69b4b95623be
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-04 20:38:56 +0100

    Add EBook class

---
+++ LibraryApp/LibraryApp/src/Domain/EBook.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryApp.Domain;
+
+public class EBook : Book
+{
+    public string FileFormat { get; }
+
+    public EBook(int id, string title, string author, string isbn, string fileFormat)
+        : base(id, title, author, isbn)
+    {
+        FileFormat = fileFormat ?? throw new ArgumentNullException(nameof(fileFormat));
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"[E-book] ID: {Id}, Tytuł: {Title}, Autor: {Author}, Format: {FileFormat}, Dostępna: {(IsAvailable ? "Tak" : "Nie")}");
+    }
+}
