664fd81e2d8c84749b996d87787b8291fe5fe507
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-04 20:38:32 +0100

    Add LibraryItem abstract class

---
+++ LibraryApp/LibraryApp/src/Domain/LibraryItem.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryApp.Domain;
+
+public abstract class LibraryItem
+{
+    public int Id { get; }
+    public string Title { get; protected set; }
+    public bool IsAvailable { get; set; } = true;
+
+    protected LibraryItem(int id, string title)
+    {
+        Id = id;
+        Title = title ?? throw new ArgumentNullException(nameof(title));
+    }
+
+    public abstract void DisplayInfo();
+}
