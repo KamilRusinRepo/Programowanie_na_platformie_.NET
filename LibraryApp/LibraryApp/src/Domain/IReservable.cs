a48edf6395c2a8b48bf93318a16d74456951b95c
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-04 20:39:16 +0100

    Add IReservable interface

---
+++ LibraryApp/LibraryApp/src/Domain/IReservable.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryApp.Domain;
+
+public interface IReservable
+{
+    void Reserve(string userEmail, DateTime from, DateTime to);
+    void CancelReservation(string userEmail);
+    bool IsAvailable();
+}   
