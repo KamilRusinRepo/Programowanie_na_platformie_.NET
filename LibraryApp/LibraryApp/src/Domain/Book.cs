b38ce2c91049b8fdc99669d2d040d0bbc287afb6
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-04 20:38:43 +0100

    Add Book class

---
+++ LibraryApp/LibraryApp/src/Domain/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryApp.Domain;
+
+public class Book : LibraryItem
+{
+    public string Author { get; }
+    public string Isbn { get; }
+
+    public Book(int id, string title, string author, string isbn) : base(id, title)
+    {
+        Author = author ?? throw new ArgumentNullException(nameof(author));
+        Isbn = isbn ?? throw new ArgumentNullException(nameof(isbn));
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"[Książka] ID: {Id}, Tytuł: {Title}, Autor: {Author}, ISBN: {Isbn}, Dostępna: {(IsAvailable ? "Tak" : "Nie")}");
+    }
+}
