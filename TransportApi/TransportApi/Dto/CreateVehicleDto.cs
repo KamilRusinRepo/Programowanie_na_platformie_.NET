613a46bc0d253d5480f862fb2d2f26eeb56e35d9
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-05 13:21:28 +0100

    Create CreateVehicleDto class

---
+++ TransportApi/TransportApi/Dto/CreateVehicleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TransportApi.Dto;
+
+public class CreateVehicleDto
+{
+    public string VehicleType { get; set; } = string.Empty; // "truck" lub "van"
+    public string RegistrationNumber { get; set; } = string.Empty;
+    public double MaxLoadKg { get; set; }
+
+    public double? TrailerLength { get; set; }
+    public double? CargoVolume { get; set; }
+}
