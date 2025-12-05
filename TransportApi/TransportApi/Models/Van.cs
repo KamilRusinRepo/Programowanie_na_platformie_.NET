c383359f518130bdcc5b98fba90628b211bfbfd2
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-05 13:20:07 +0100

    Create Van class

---
+++ TransportApi/TransportApi/Models/Van.cs
@@ -0,0 +1,9 @@
+﻿namespace TransportApi.Models;
+
+public class Van : Vehicle, IReservable
+{
+    public double CargoVolume { get; set; }
+
+    public override string GetInfo() =>
+        $"Van {RegistrationNumber} - MaxLoad: {MaxLoadKg}kg, Volume: {CargoVolume}m3, Available: {IsAvailable}";
+}
