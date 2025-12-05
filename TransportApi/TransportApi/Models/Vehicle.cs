a10323b001f9e3e5e2d7fac8812cb32eb8516502
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-05 13:19:56 +0100

    Create Vehicle class

---
+++ TransportApi/TransportApi/Models/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿namespace TransportApi.Models;
+
+public abstract class Vehicle : IReservable
+{
+    public int Id { get; set; }
+    public string RegistrationNumber { get; set; } = string.Empty;
+    public double MaxLoadKg { get; set; }
+    public bool IsAvailable { get; set; } = true;
+
+    public abstract string GetInfo();
+    public void StartOrder() => IsAvailable = false;
+    public void CompleteOrder() => IsAvailable = true;
+}
