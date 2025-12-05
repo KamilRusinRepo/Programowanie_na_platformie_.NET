fffed6f273f97a24ad5c08dd5afd9eb9436990dc
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-05 13:21:57 +0100

    Create TransportOrderExtension class

---
+++ TransportApi/TransportApi/Extensions/TransportOrderExtension.cs
@@ -0,0 +1,11 @@
+﻿using TransportApi.Models;
+
+namespace TransportApi.Extensions;
+
+public static class TransportOrderExtension
+{
+    public static IQueryable<TransportOrder> Active(this IQueryable<TransportOrder> query)
+    {
+        return query.Where(o => !o.IsCompleted);
+    }
+}
