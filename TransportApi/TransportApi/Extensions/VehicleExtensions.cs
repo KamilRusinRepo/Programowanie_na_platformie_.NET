b66169d3994f5c6a6d4c4d0acbf6017c61318738
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-05 13:21:44 +0100

    Create VehicleExtensions class

---
+++ TransportApi/TransportApi/Extensions/VehicleExtensions.cs
@@ -0,0 +1,11 @@
+﻿using TransportApi.Models;
+
+namespace TransportApi.Extensions;
+
+public static class VehicleExtensions
+{
+    public static IQueryable<Vehicle> GetAvailableVehicles(this IQueryable<Vehicle> query)
+    {
+        return query.Where(v => v.IsAvailable);
+    }
+}
