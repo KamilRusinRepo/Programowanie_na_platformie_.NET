3b8000b7525ee108afc724709a43a155859a9721
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-05 13:20:20 +0100

    Create Truck class

---
+++ TransportApi/TransportApi/Models/Truck.cs
@@ -0,0 +1,9 @@
+﻿namespace TransportApi.Models;
+
+public class Truck : Vehicle, IReservable
+{
+    public double TrailerLength { get; set; }
+
+    public override string GetInfo() =>
+        $"Truck {RegistrationNumber} - MaxLoad: {MaxLoadKg}kg, TrailerLength: {TrailerLength}m, Available: {IsAvailable}";
+}
