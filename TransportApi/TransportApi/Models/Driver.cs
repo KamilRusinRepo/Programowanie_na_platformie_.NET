c19a9b0799cd4bd4f45e2e06dc09127ce961c79d
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-05 13:19:43 +0100

    Create Driver class

---
+++ TransportApi/TransportApi/Models/Driver.cs
@@ -0,0 +1,12 @@
+﻿namespace TransportApi.Models;
+
+public class Driver :  IReservable
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string LicenseNumber { get; set; } = string.Empty;
+    public bool IsAvailable { get; set; } = true;
+    
+    public void StartOrder() => IsAvailable = false;
+    public void CompleteOrder() => IsAvailable = true;
+}
