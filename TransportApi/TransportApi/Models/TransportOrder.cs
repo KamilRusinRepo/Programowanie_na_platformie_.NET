a78fca6ea045ee0df5f6edc9d6fe7b465f5ea364
Kamil Rusin <kamil.rusin15@wp.pl>
2025-12-05 13:21:10 +0100

    Create TransportOrder class

---
+++ TransportApi/TransportApi/Models/TransportOrder.cs
@@ -0,0 +1,16 @@
+﻿namespace TransportApi.Models;
+
+public class TransportOrder
+{
+    public int Id { get; set; }
+    public string CargoDescription { get; set; } = string.Empty;
+    public double Weight { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public bool IsCompleted { get; set; } = false;
+
+    public int VehicleId { get; set; }
+    public Vehicle? Vehicle { get; set; }
+
+    public int DriverId { get; set; }
+    public Driver? Driver { get; set; }
+}
